3d3238c0c7bc68ca7aaca677c7083d2fb8edc94a
Zhenya Aleksanyan <alexanyan.jenya@gmail.com>
2023-06-16 11:31:17 +0400

    Updated project

--- index.js
+++ index.js
@@ -1,61 +1,43 @@
 import fs from "fs";
-import process from "process";
-import { parse } from "csv-parse";
-import cluster from "cluster";
 import os from "os";
+import path from "path";
+import process from "process";
+import cluster from "cluster";
+import csvToJson from "./parser.js";
+import divPathArr from './divide.js';
+
 
 const input = process.argv[process.argv.length - 1];
 
-fs.readdir(input, { recursive: true }, (err, files) => {
-  if (err) {
-    console.log(err.message);
-  } else {
-    files.forEach((item) => {
-      if (cluster.isPrimary) {
-        for (let i = 0; i < os.cpus().length - 1; i++) {
-          const worker = cluster.fork();
-        }
-      } else {
-        console.log("Worker is running", process.pid);
-        const old = new Date().getMilliseconds();
-        const currentDir = process.cwd();
-        console.log(currentDir);
-        const fileName = item.slice(0, item.length - 4);
+if(cluster.isPrimary) {
+  const numCPUs = os.cpus().length;
 
-        const outputPath = `${currentDir}/converted/${fileName}.json`;
-
-        const readableStream = fs.createReadStream(
-          `${currentDir}/${input}/${item}`
-        );
-        const writableStream = fs.createWriteStream(outputPath);
-
-        readableStream.on("error", (error) => {
-          console.log("Error with the readable stream:", error.message);
-          process.exit(1);
-        });
-
-        writableStream.on("error", (error) => {
-          console.log("Error with the writable stream:", error.message);
-          process.exit(1);
-        });
-
-        const parser = parse();
-        parser.on("readable", () => {
-          let data;
-          while ((data = parser.read())) {
-            const jsonData = JSON.stringify(data);
-            writableStream.write(`${jsonData}\n`);
-          }
-        });
-
-        parser.on("end", () => {
-          writableStream.end();
-          const newData = new Date().getMilliseconds();
-          console.log(newData - old);
-        });
-
-        readableStream.pipe(parser);
+  const csvFiles = new Promise((resolve, reject) => {
+    fs.readdir(input, { recursive: true }, (err, files) => {
+      if (err)
+        reject(err.message);
+      else {
+        files.forEach(file => {
+          if (path.extname(file) == ".csv")
+            resolve(files);
+        })
       }
-    });
-  }
-});
+    })
+  });
+
+  csvFiles.then(res => {
+    const workerCount = Math.min(numCPUs, res.length);
+    const csvFilePath = divPathArr(res, workerCount)
+    for (let i = 0; i < csvFilePath.length; i++) {
+      const worker = cluster.fork({ file: csvFilePath[i] });
+      console.log('worker:', worker.id);
+    }
+  }).catch(err => {
+    console.log(err);
+  })
+  
+} else {
+  const arr = process.env.file.split(',');
+  csvToJson(input, arr);
+}
+
